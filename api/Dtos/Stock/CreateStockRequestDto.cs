51c9849b62eeb6d208c9838765af3ce65dcdc166
alexey <tuchnyy@list.ru>
2025-03-16 18:17:35 +0300

    Added validation attributes

--- api/Dtos/Stock/CreateStockRequestDto.cs
+++ api/Dtos/Stock/CreateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,25 @@ namespace api.Dtos.Stock
 {
     public class CreateStockRequestDto
     {
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [StringLength(10, ErrorMessage = "Длина строки не должна превышать 10 символов")]
         public string Symbol { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [StringLength(30, ErrorMessage = "Длина строки не должна превышать 30 символов")]
         public string CompanyName { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [Range(0, int.MaxValue)]
         public decimal Purchase { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public decimal LastDiv { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string Industry { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public long MarketCap { get; set; }
     }
 }
--- api/Dtos/Stock/UpdateStockRequestDto.cs
+++ api/Dtos/Stock/UpdateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
@@ -8,20 +9,25 @@ namespace api.Dtos.Stock
 {
     public class UpdateStockRequestDto
     {
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public Guid Id { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string Symbol { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string CompanyName { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public decimal Purchase { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public decimal LastDiv { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string Industry { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public long MarketCap { get; set; }
-
-        public List<Comment> Comments { get; set; }
     }
 }
